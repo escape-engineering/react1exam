0631e3595098df0b2c0cf0533dc3778b76e0fc31
escape-engineering <117638805+escape-engineering@users.noreply.github.com>
2022-12-01 10:59:48 +0900

    기능작동

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Todos({ todo }) {
+  return <div className="todos">{todo.title}</div>;
+}
+
 function App() {
+  const [todos, setTodos] = useState([
+    { title: '리액트를 배워봅시다.', id: 0 },
+  ]);
+
+  const [title, setTitle] = useState('');
+
+  const addTitleHandler = () => {
+    const lastid = todos[todos.length - 1].id;
+    const newTodo = { title: title, id: lastid + 1 };
+    setTodos([...todos, newTodo]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <div className="header">
+        <div>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <button onClick={addTitleHandler}>추가하기</button>
+        </div>
+      </div>
+      <div className="header">
+        <h1>Todo List</h1>
+      </div>
+      <div className="todo-list">
+        {todos.map((todo) => {
+          return <Todos todo={todo} key={todo.id} />;
+        })}
+      </div>
     </div>
   );
 }
